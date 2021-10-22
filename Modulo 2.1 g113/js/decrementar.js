8f46266d8b8b107aa0ad624782e0e1d1b5b46111
taguivalle <taguivalle140965@gmail.com>
2021-10-22 15:16:38 -0500

    Initial commit

---
+++ Modulo 2.1 g113/js/decrementar.js
@@ -0,0 +1,7 @@
+
+ var clicks = 0;
+
+ function decrementar() {
+   clicks = clicks - 1;
+   document.getElementById('restador').innerHTML = clicks;
+ }
---
+++ Modulo 2.1 g113/js/incrementar.js
@@ -0,0 +1,19 @@
+
+        var clicks = 0;
+
+        function incrementar() {
+            clicks = clicks + 1;
+            document.getElementById('contador').innerHTML = clicks;
+        }
+
+			// $(function(){
+			// $("#mensaje1").click(function(){
+			// 	$("#contenido").load("email1.txt");
+			// });
+			// $("#mensaje2").click(function(){
+			// 	$("#contenido").load("email2.txt");
+			// });
+			// $("#mensaje3").click(function(){
+			// 	$("#contenido").load("email3.txt");
+			// });
+			// });
